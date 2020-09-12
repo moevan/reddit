7d0d32d665eaf88f5d2358b6d61dff58d1382c12
Thị Moè <vanlt@shopdunk.vn>
2020-09-12 14:32:16 +0700

    finish

--- public/script.js
+++ public/script.js
@@ -5,41 +5,44 @@
 const dataDisplayer = document.getElementById("dataDisplayer");
 const loadPostForm = document.querySelector("form");
 const loading = document.getElementById("loading");
-
+const clear = document.getElementById("clear");
+clear.addEventListener("click",()=>{
+ fetch("/clear");
+ loadPostForm.reset();
+ dataDisplayer.innerText = "";
+})
 // listen for the form to be submitted and add a new dream when it is
 loadPostForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  loading.classList.remove("none");
-  const dayInput =  new Date(document.getElementById("dayInput").value).getTime()/1000;
-  const subredditInput = document.getElementById("subredditInput").value;
+    event.preventDefault();
+    loading.classList.remove("none");
+    const dayInput =
+        new Date(document.getElementById("dayInput").value).getTime() / 1000;
+    const subredditInput = document.getElementById("subredditInput").value;
 
-  fetch("/posts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ dayInput, subredditInput }),
-  }).then((res) => {
-    
-    res.json().then((posts) => {
+    fetch("/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ dayInput, subredditInput }),
+    }).then((res) => {
+        res.json().then((posts) => {
+            if (posts.length > 0) {
+                posts.forEach((post) => {
+                    let node = document.createElement("li");
+                    node.className = "item";
 
-console.log(posts);
-if (posts.length > 0){
-  posts.forEach((post) => {
-        
-    let node = document.createElement("li");
-    node.className = 'item';
-     node.innerHTML =  `<a href="https://reddit.com${post.data.permalink}"><img src="${post.data.thumbnail}"/></a>` ;
-     dataDisplayer.appendChild(node);
-     loading.classList.add("none");
-   })
-} else {
-  dataDisplayer.innerText = "Found nothing";
-  loading.classList.add("none");
-}
+                    node.innerHTML = `<a href="https://reddit.com${post.data.permalink}"><img src="${post.data.thumbnail}"/></a>`;
+                    dataDisplayer.appendChild(node);
+                });
+            } else {
+                let node = document.createElement("li");
+                node.className = "item";
+                node.innerText = "Found nothing, no more result";
+                dataDisplayer.appendChild(node);
+            }
 
-    
-    }
-    );
-  });
+            loading.classList.add("none");
+        });
+    });
 });
--- server.js
+++ server.js
@@ -5,136 +5,130 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
-let before = '';
-let after = '';
+let before = "";
+let after = "";
 const fetch = require("node-fetch");
 const {
-  updateSavedToken,
-  getCurrentToken,
-  connectToMongoDB,
-  insertNewObject,
-  getObj,
-  clearDatabase,
+    updateSavedToken,
+    getCurrentToken,
+    connectToMongoDB,
+    insertNewObject,
+    getObj,
+    clearDatabase,
 } = require("./mongo");
 const { isTokenValid, getNewToken } = require("./auth");
 const moment = require("moment"); // require
 let token;
 
+async function isAccCreatedAfterDayX(username, dayX) {
+    if (username == "[deleted]") {
+        return true;
+    } else {
+        async function getUserAge() {
+            let res = await fetch(
+                `https://oauth.reddit.com/user/${username}/about`,
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                    },
+                }
+            );
+            let json = await res.json();
 
+            return json.data.created_utc;
+        }
+        const timeCreated = await getUserAge();
 
-async function isAccCreatedAfterDayX(username,dayX) {
-
-  if (username == '[deleted]'){
-
-    return true;
-    
-  } else {
-    async function getUserAge() {
-      let res = await fetch(`https://oauth.reddit.com/user/${username}/about`, {
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      });
-      let json = await res.json();
-
-      return json.data.created_utc;
+        return timeCreated > dayX;
     }
-    const timeCreated = await getUserAge();
-
-    return timeCreated > dayX;
-  }
-  
 }
-  
-async function filterPosts(posts,dayX){
-        
-      let j = 1;
-      let filtered = [];
-      for (i = 0; i < posts.length ; i++) {
+
+async function filterPosts(posts, dayX) {
+    let j = 1;
+    let filtered = [];
+    for (i = 0; i < posts.length; i++) {
         var post = posts[i];
         let res = await isAccCreatedAfterDayX(post.data.author, dayX);
-     
-        if (res &&  post.data.thumbnail.includes("http")) {
-          
-          filtered.push({
-            _id: j,
-            data: {
-            
-             permalink: post.data.permalink,
-              author: post.data.author,
-              thumbnail: post.data.thumbnail
-            }
-          })
-        
-          j++;
+
+        if (res && post.data.thumbnail.includes("http")) {
+            filtered.push({
+                _id: j,
+                data: {
+                    permalink: post.data.permalink,
+                    author: post.data.author,
+                    thumbnail: post.data.thumbnail,
+                },
+            });
+
+            j++;
         }
-      }
-    
-      return filtered;
+    }
+
+    return filtered;
 }
 
 async function getNewPosts(subreddit) {
-  let myCurrentToken = await getCurrentToken();
-  let res = await fetch(
-    `https://oauth.reddit.com/r/${subreddit}/new?limit=4&after=${after}`,
-    {
-      headers: {
-        Authorization: "Bearer " + myCurrentToken,
-      },
-    }
-  );
-  let json = await res.json();
-before = json.data.before;
-after = json.data.after;
-  let posts = json.data.children;
-return posts;
- 
-
- 
+    let myCurrentToken = await getCurrentToken();
+    let res = await fetch(
+        `https://oauth.reddit.com/r/${subreddit}/new?limit=10&after=${after}`,
+        {
+            headers: {
+                Authorization: "Bearer " + myCurrentToken,
+            },
+        }
+    );
+    let json = await res.json();
+    before = json.data.before;
+    after = json.data.after;
+    let posts = json.data.children;
+    return posts;
 }
 async function handleRedditToken() {
-  // test token is valid or not
-  const isTokenValidTrue = await isTokenValid();
-  if (isTokenValidTrue) {
-    token = await getCurrentToken();
-  } else {
-    // refresh token and update in database
-    token = await getNewToken();
-    updateSavedToken(token);
-  }
+    // test token is valid or not
+    const isTokenValidTrue = await isTokenValid();
+    if (isTokenValidTrue) {
+        token = await getCurrentToken();
+    } else {
+        // refresh token and update in database
+        token = await getNewToken();
+        updateSavedToken(token);
+    }
 }
-
-
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 // listen for requests :)
 const listener = app.listen(8080, () => {
-  console.log("Your app is listening on port " + 8080);
+    console.log("Your app is listening on port " + 8080);
 });
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+    response.sendFile(__dirname + "/views/index.html");
+    console.log(after);
 });
 
 // send the default array of dreams to the webpage
-app.post("/posts", jsonParser,(request, response) => {
-  async function main(requestBody,response) {
+app.post("/posts", jsonParser, (request, response) => {
   
-    //  connect to db
-    await connectToMongoDB();
-    await handleRedditToken();
-    // do waht i want
-    // clearDatabase();
-    const posts = await getNewPosts(requestBody.subredditInput);
-    let filtered = await filterPosts(posts,parseInt(requestBody.dayInput));
-    
-    response.json(filtered);
-    
-  }
-  main(request.body,response);
+    async function main(requestBody, response) {
+        //  connect to db
+        await connectToMongoDB();
+        await handleRedditToken();
+        // do waht i want
+        // clearDatabase();
+        const posts = await getNewPosts(requestBody.subredditInput);
+        let filtered = await filterPosts(posts, parseInt(requestBody.dayInput));
+
+        response.json(filtered);
+    }
+    main(request.body, response);
 });
+
+app.get("/clear",(request, response)=>{
+  after = '';
+  before = '';
+})
